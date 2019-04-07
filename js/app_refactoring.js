8db78c7008053ff4dcfc123e67a01448384e87aa
Vinicius <vinicius@vweisscobos.com>
2019-04-07 19:11:47 -0300

    create messages class

--- js/app_refactoring.js
+++ js/app_refactoring.js
@@ -20,3 +20,44 @@ class GameObserver {
     this.subscribers.push([event, func]);
   }
 }
+
+/* Messages class provides the feature to display messages in the canvas and
+ * is also responsible to remove then after a while.
+ */
+class Messages {
+
+  constructor() {
+    this.messages = [];
+  }
+
+  newMessage(type, x, y) {
+    this.messages.push({type, x, y, time: Date.now()});
+  };
+
+  //  remove messages which are visible for more than 0.5s
+  update() {
+    let messagesToRemove = 0;
+
+    this.messages.forEach(msg => {
+      if (Date.now() - msg.time >  500) messagesToRemove++;
+    });
+
+    this.removeOldMessages(messagesToRemove)
+  };
+
+  removeOldMessages(qnt) {
+    for (let i = 0; i < qnt; i++) {
+      this.messages.shift();
+    }
+  };
+
+  render() {
+    this.messages.forEach(msg => {
+      GLOBALS.ctx.drawImage(
+        Resources.get(GLOBALS.messageSprites.get(msg.type)),
+        msg.x,
+        msg.y
+      );
+    });
+  };
+}
